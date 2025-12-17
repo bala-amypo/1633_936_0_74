4cd7d8ae3af27ea7fd9831a28c1a821f22e20f2a
bala-amypo <bala@amypo.in>
2025-12-17 09:19:07 +0000

    User commit

--- src/main/java/com/service/impl/StudentServicelmpl.java
+++ src/main/java/com/service/impl/StudentServicelmpl.java
@@ -3,5 +3,6 @@ package com.example.demo.service.Impl;
 import org.springframework.stereotype.service;
 @service
 public class Studentserverimpl implement StudentService{
+    @Autowired StudentRepository Student;
     
 }
