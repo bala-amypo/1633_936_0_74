ccab1fd4a5a741b70f5579cad020f1361d71f3b5
bala-amypo <bala@amypo.in>
2025-12-18 06:29:24 +0000

    User commit

--- src/main/java/com/service/impl/StudentServicelmpl.java
+++ src/main/java/com/service/impl/StudentServicelmpl.java
@@ -1,37 +1,44 @@
 package com.example.demo.service.Impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.service.StudentService;
+import java.util.List;
+import com.example.demo.repository.StudentRepo;
+@Service
 
-import org.springframework.stereotype.service;
-@service
-public class Studentserverimpl implement StudentService{
-    @Autowired StudentRepository Student;
-    
-    
-}
-@Override
-public String DeleteDtata(            )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@Override
-public StudentEntity getData(int id){
-    return student.findById(id).orElse(null);
-
+public class StudentServiceImpl implements StudentService{
+    @Autowired StudentRepo student;
+    //save()
+    //findAll()
+    //findById()
+    //deleteById();
+    //existById();
+    @Override
+    public StudentEntity postData(StudentEntity stu){
+        //StudentEntity getData();
+        return student.save(stu);
+    }
+    @Override
+    public List<StudentEntity>getAllData(){
+        return student.findAll();
+    }
+    @Override
+    public String DeleteData(int id){
+        student.deleteById(id);
+        return "Deleted Successfully";
+    }
+    @Override
+    public StudentEntity getData(int id){
+        return student.findById(id).orElse(null);
+    }
+    @Override
+    public StudentEntity updateData(int id,StudentEntity entity){
+        if(student.existsById(id)){
+            entity.setId(id);
+            return student.save(entity);
+        }
+        return null;
+    }
 }
