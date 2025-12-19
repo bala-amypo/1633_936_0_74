ec6b8f849f644bd57c9ca0384855e9f211d9f743
bala-amypo <bala@amypo.in>
2025-12-19 06:16:05 +0000

    User commit

--- src/main/java/exception/GlobalException.java
+++ src/main/java/exception/GlobalException.java
@@ -6,19 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Service
-public class ValidationServiceImpl  implements ValidationService{
-@Autowired ValidationRepo student; 
-
-@Override
-public ValidationEntity postData(ValidationEntity stu){
-    return student.save(stu);
-}
-@Override
-public ValidationEntity getStudentById(Integer id){
-    return repo.findById(id).orElseThrow(()->new ValidationException("Invalid Id"+id));
-}
-}
 
 
 
