d26feacc9523b9fcae182e86d2d474faeca335a7
bala-amypo <bala@amypo.in>
2025-12-19 10:48:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Timestamp.java
+++ src/main/java/com/example/demo/entity/Timestamp.java
@@ -1,23 +1,12 @@
 package com.example.demo.entity;
 
 import java.time.LocalDateTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-
 import java.util.*;
+
+
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class TimeStampEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -31,7 +20,7 @@ public class TimeStampEntity{
     public void Oncreated(){
         LocalDataTime now=LocalDateTime.now();
        this.createdAt=now;
-       this.updateAt=now;
+       
     }
     @PreUpdate
     public void Onupdate(){
